f4c4e5ae55fbd5e8e22a2c32e7cd1527bdda6187
ip311s05 <vlasova.katya2005@gmail.com>
2026-09-14 18:09:23 +0700

    lab1:Add interface and function Whos_That_Zodiak

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,6 +12,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.Hello).text = "Hello world from Katya and Anton"
+//        findViewById<TextView>(R.id.Hello).text = "Hello world from Katya and Anton"
+
+    }
+    private fun Whos_That_Zodiak(day: Int, month: Int): String {
+        return when (month) {
+            1 -> if (day < 20) "Козерог" else "Водолей"
+            2 -> if (day < 19) "Водолей" else "Рыбы"
+            3 -> if (day < 21) "Рыбы" else "Овен"
+            4 -> if (day < 20) "Овен" else "Телец"
+            5 -> if (day < 21) "Телец" else "Близнецы"
+            6 -> if (day < 21) "Близнецы" else "Рак"
+            7 -> if (day < 23) "Рак" else "Лев"
+            8 -> if (day < 23) "Лев" else "Дева"
+            9 -> if (day < 23) "Дева" else "Весы"
+            10 -> if (day < 23) "Весы" else "Скорпион"
+            11 -> if (day < 22) "Скорпион" else "Стрелец"
+            12 -> if (day < 22) "Стрелец" else "Козерог"
+            else -> ""
+        }
     }
 }
+
