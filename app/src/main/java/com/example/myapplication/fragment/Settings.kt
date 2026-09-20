26fc70f1b35f93ea5dbf270e8178fe9a3c0063bc
Anton Muravev <anton.m3451@gmail.com>
2026-09-21 01:57:27 +0700

    Создана страница *Настройки*

--- app/src/main/java/com/example/myapplication/fragment/Settings.kt
+++ app/src/main/java/com/example/myapplication/fragment/Settings.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
@@ -12,5 +14,19 @@ class Settings : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val seekBar = view.findViewById<SeekBar>(R.id.ScrollSpeedGame)
+        val textView = view.findViewById<TextView>(R.id.ShowSpeedGame)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView.text = "${(progress + 1) * 10}%"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
